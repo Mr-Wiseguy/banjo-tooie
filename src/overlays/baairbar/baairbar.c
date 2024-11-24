495293f85b8643d9e9b77e7ddb1ec8bafd7de612
banjo.decomp <banjo.decomp@gmail.com>
2024-11-24 16:12:43 -0600

    match: overlay/baairbar.c

--- src/overlays/baairbar/baairbar.c
+++ src/overlays/baairbar/baairbar.c
@@ -1,3 +1,27 @@
 #include "common.h"
 
-#pragma GLOBAL_ASM("asm/nonmatchings/overlays/baairbar/baairbar/baairbar_entrypoint_0.s")
+#include "ba/playerstate.h"
+
+void baairbar_entrypoint_0(PlayerState *self, s32 arg1) {
+    if (func_800DA298(0xE9) && _gclevel_entrypoint_4(0x10)) {
+        return;
+    }
+    
+    if (!func_8008DAA8(self)) {
+        return;
+    }
+
+    if (func_800F8B88() == 3) {
+        return;
+    }
+
+    switch (arg1) {
+        case 1:
+            func_800CF494();
+            break;
+
+        case 2:
+            func_800CF2D4(1);
+            break;
+    }
+}
