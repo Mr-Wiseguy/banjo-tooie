212c5a3d3548ff5a5f0432415ef0aa4fb0dbdc1e
Bl00D4NGEL <github@d-peters.com>
2025-01-29 21:39:08 +0100

    match: overlays/bskazpaddle.c

--- include/bs/drone.h
+++ include/bs/drone.h
@@ -6,5 +6,7 @@
 extern void _bsdrone_entrypoint_0(PlayerState *self);
 extern void _bsdrone_entrypoint_1();
 extern void _bsdrone_entrypoint_2();
+extern s32 _badrone_entrypoint_24(PlayerState *self);
+extern s32 _badrone_entrypoint_30();
 
 #endif // __BS_DRONE_H__
