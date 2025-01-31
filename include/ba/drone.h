1a97e11e526b713a95b09e781e675322fe4da87b
Bl00D4NGEL <github@d-peters.com>
2025-01-31 21:49:07 +0100

    fix: make

--- include/ba/drone.h
+++ include/ba/drone.h
@@ -4,9 +4,6 @@
 #include "playerstate.h"
 #include "bs/state.h"
 
-extern void _bsdrone_entrypoint_0(PlayerState *self);
-extern void _bsdrone_entrypoint_1();
-extern void _bsdrone_entrypoint_2(PlayerState *self);
 extern s32 _badrone_entrypoint_3(PlayerState *self);
 extern BanjoStateId _badrone_entrypoint_23(PlayerState *self, f32[3]);
 extern BanjoStateId _badrone_entrypoint_24(PlayerState *self);
