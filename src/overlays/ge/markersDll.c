f3da40e66988a1e734faa4849827e53fa0275d3b
Dominik Peters <github@d-peters.com>
2025-06-10 07:21:20 +0200

    Restructure overlay files (#21)
    * refac: change structure for overlays/anseq.c and overlays/gemarkersDll
    
    * refac: replace shell call with a list of include dirs that get -I prefixed

---
+++ src/overlays/ge/markersDll.c
@@ -0,0 +1,5 @@
+#include "markersDll.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/ge/markersDll/gemarkersDll_entrypoint_0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/overlays/ge/markersDll/gemarkersDll_entrypoint_1.s")
