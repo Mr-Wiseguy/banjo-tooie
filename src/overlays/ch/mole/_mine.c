58cd39699b64de2abc4922ebb68a8bd19b10e2b2
Zenpock <70054140+Zenpock@users.noreply.github.com>
2025-09-24 16:28:14 -0500

    Matched duplicate functions in mole overlays

--- src/overlays/ch/mole/_mine.c
+++ src/overlays/ch/mole/_mine.c
@@ -1,9 +1,27 @@
-#include "common.h"
+#include "ch/mole.h"
 
-#pragma GLOBAL_ASM("asm/nonmatchings/overlays/ch/mole/_mine/func_80800000_chmole_mine.s")
+extern MoveData D_808000C0_chmole_mine[];
+extern D_808000D0 D_808000E0_chmole_mine;
 
-#pragma GLOBAL_ASM("asm/nonmatchings/overlays/ch/mole/_mine/func_80800024_chmole_mine.s")
+void func_80800000_chmole_mine(Actor* a0, u32 a1, u32 a2)
+{
+	_sumole_entrypoint_3(a0, a1, a2, D_808000C0_chmole_mine);
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/overlays/ch/mole/_mine/func_80800090_chmole_mine.s")
+void func_80800024_chmole_mine(Actor* a0)
+{
+	_sumole_entrypoint_0(a0, 0);
+	a0->unk74_7 = a0->unk74_7 - 0x32;
+	a0->unk54 = a0->unk74_7;
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/overlays/ch/mole/_mine/chmole_mine_entrypoint_0.s")
+void func_80800090_chmole_mine(Actor* a0)
+{
+	_sumole_entrypoint_4(a0, D_808000C0_chmole_mine);
+}
+
+void* chmole_mine_entrypoint_0()
+{
+	return &D_808000E0_chmole_mine;
+}
+
--- src/overlays/ch/mole/_witchy.c
+++ src/overlays/ch/mole/_witchy.c
@@ -1,9 +1,27 @@
-#include "common.h"
+#include "ch/mole.h"
 
-#pragma GLOBAL_ASM("asm/nonmatchings/overlays/ch/mole/_witchy/func_80800000_chmole_witchy.s")
+extern MoveData D_808000C0_chmole_witchy[];
+extern D_808000D0 D_808000F0_chmole_witchy;
 
-#pragma GLOBAL_ASM("asm/nonmatchings/overlays/ch/mole/_witchy/func_80800024_chmole_witchy.s")
+void func_80800000_chmole_witchy(Actor* a0, u32 a1, u32 a2)
+{
+	_sumole_entrypoint_3(a0, a1, a2, D_808000C0_chmole_witchy);
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/overlays/ch/mole/_witchy/func_80800090_chmole_witchy.s")
+void func_80800024_chmole_witchy(Actor* a0)
+{
+	_sumole_entrypoint_0(a0, 0);
+	a0->unk74_7 = a0->unk74_7 - 0x32;
+	a0->unk54 = a0->unk74_7;
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/overlays/ch/mole/_witchy/chmole_witchy_entrypoint_0.s")
+void func_80800090_chmole_witchy(Actor* a0)
+{
+	_sumole_entrypoint_4(a0, D_808000C0_chmole_witchy);
+}
+
+void* chmole_witchy_entrypoint_0()
+{
+	return &D_808000F0_chmole_witchy;
+}
+
