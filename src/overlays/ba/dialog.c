b7bf829442cad99fbc4d60190bc709bee1a716b2
Zenpock <70054140+Zenpock@users.noreply.github.com>
2025-09-24 16:51:53 -0500

    Match empty funcs with 3 args

--- src/overlays/ba/dialog.c
+++ src/overlays/ba/dialog.c
@@ -2,8 +2,8 @@
 
 #pragma GLOBAL_ASM("asm/nonmatchings/overlays/ba/dialog/badialog_entrypoint_0.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/overlays/ba/dialog/badialog_entrypoint_1.s")
-
+void badialog_entrypoint_1(s32 arg0, s32 arg1, s32 arg2) {
+}
 s32 badialog_entrypoint_2(s32 arg0, s32 arg1, s32 arg2) 
 {
     return 0;
